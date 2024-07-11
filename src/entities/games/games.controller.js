d654960a6ed42f7acc13e60570090f13ea96c9fb
datata <dani@danitarazona.dev>
2024-07-11 11:50:11 +0200

    feat: delete game by id

--- src/entities/games/games.controller.js
+++ src/entities/games/games.controller.js
@@ -8,7 +8,7 @@ export const createGame = async (req, res) => {
     if( !title || !description) {
        throw new Error('Title and description are required')
     }
-    
+
     const newGame = await Game.create({
       title: title,
       description: description
@@ -41,3 +41,57 @@ export const createGame = async (req, res) => {
     )
   }
 } 
+
+export const getAllGames = async (req, res) => {
+  try {
+    const games = await Game.find()
+
+    res.status(200).json(
+      {
+        success: true,
+        message: "Games retrieved successfully",
+        data: games
+      }
+    )
+
+  } catch (error) {
+    res.status(500).json(
+      {
+        success: false,
+        message: "Error retrieving games",
+        error: error.message
+      }
+    )
+  }
+}
+
+export const deleteGame= async(req, res) =>{
+  try {
+      const idDelete= req.params.id
+      const idToDeleteValid= Types.ObjectId.isValid(idDelete)
+      if(!idToDeleteValid){
+          return res.status(400).json({
+              success:false,
+              message:"Id not valid"
+          })
+      }
+      const deletedGame =await Game.findByIdAndDelete(idDelete)
+      if(!deletedGame){
+          return res.status(404).json({
+              succes:false,
+              message:"Not Game found"
+          })
+      }
+      res.status(200).json({
+          success:true,
+          message:"Game deleted",
+      })
+      
+  } catch (error) {
+      return res.status(500).json({
+          success:false,
+          message:"Error deleting games",
+          error:error.message
+      })
+  }
+}
--- src/entities/games/games.routes.js
+++ src/entities/games/games.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
-import { createGame } from './games.controller.js'
+import { createGame, deleteGame, getAllGames } from './games.controller.js'
 
 const router = Router()
 
 router.post('/', createGame)
+router.get('/', getAllGames)
+router.delete('/:id', deleteGame)
 
 export { router }
